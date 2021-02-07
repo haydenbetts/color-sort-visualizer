a1185dcf87acbc6210368c7827493f72f875ca23
Hayden Betts <haydenbetts@gmail.com>
2021-02-06 23:02:29 -0500

    Paint on mobile

--- src/paint.js
+++ src/paint.js
@@ -11,10 +11,13 @@ class Paint {
     this.canvas.onmousedown = this.mouseDown.bind(this);
     this.canvas.onmouseup = this.mouseUp.bind(this);
     this.canvas.onmousemove = this.mouseMove.bind(this);
+
+    this.canvas.ontouchstart = this.mouseDown.bind(this);
+    this.canvas.ontouchend = this.mouseUp.bind(this);
+    this.canvas.ontouchmove = this.mouseMove.bind(this);
   }
 
   mouseDown(e) {
-    console.log(e);
     this.isDragging = true;
     this.startPoint.x = e.offsetX;
     this.startPoint.y = e.offsetY;
@@ -28,11 +31,17 @@ class Paint {
   }
 
   mouseMove(e) {
-    console.log(this.context);
-    if (this.isDragging) {
+    if (!this.isDragging) return;
+
+    if (e instanceof TouchEvent) {
+      this.context.lineTo(
+        e.touches[0].pageX - e.touches[0].target.offsetLeft,
+        e.touches[0].pageY - e.touches[0].target.offsetTop
+      );
+    } else {
       this.context.lineTo(e.offsetX, e.offsetY);
-      this.context.stroke();
     }
+    this.context.stroke();
   }
 }
 
